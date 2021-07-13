8187f17e4cfe7fa2d6dee3d5e2805becc8e4676b
Malcolm Ke Win <kexianbin@diyism.com>
2021-07-14 01:33:07 +0800

    Create test.go
    forkgo allman style testing

---
+++ forkgo-test/test.go
@@ -0,0 +1,18 @@
+package main
+import
+(
+    "fmt"
+)
+
+
+func main()/
+{
+    if false/
+    {   fmt.Println("jack")
+        fmt.Println("forkgo")
+    } else/
+    {   fmt/
+           .Println("hello")
+        fmt.Println("forkgo")
+    }
+}
