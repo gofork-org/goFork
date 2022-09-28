cc7441cd5ed6c40ae48a58d73ed01a439b889b9f
Malcolm <kexianbin@diyism.com>
2022-09-28 20:30:13 +0800

    add the test-gofork

---
+++ test-gofork/test.go
@@ -0,0 +1,22 @@
+package main
+import
+(   "fmt"
+)
+
+
+func main()
+{   if false
+    {   fmt.Println("jack")
+        fmt.Println("gofork")
+    } else
+    {   fmt/
+          .Println("hello")
+        fmt.Println("gofork")
+    }
+
+    var a="hello"
+
+    {   var b="gofork"
+        fmt.Println(a, b)
+    }
+}
